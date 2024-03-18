6752dfe7c92b692daf1ff4853b2082f8051d7711
AmulyaMat <143415536+AmulyaMat@users.noreply.github.com>
2024-03-17 23:54:47 -0400

    comments update q1_3.py

--- gan/q1_3.py
+++ gan/q1_3.py
@@ -16,7 +16,19 @@ def compute_discriminator_loss(
     # TODO 1.3: Implement GAN loss for discriminator.
     # Do not use discrim_interp, interp, lamb. They are placeholders
     # for Q1.5.
+    ####
+    # discrim_real and discrim_fake are each a tensor with values of the predictions
+    # discrim_real is a tensor with predictions when REAL images are input into the discriminator --> ideally 1
+    # discrim_fake is a tensor with predictions when FAKE images are output from the generator and 
+    # passed into the discriminator --> ideally 0
+    # 0 means fake, 1 means real  --> raw logits
+
+    # for discriminator, the total loss shows the overall ability of the discriminator to differentiate
+    # between fake and real images
+    # real loss is to compare predictions of real images with tensor of ones (ideal)
+    # fake loss is to compare predictions of fake images with tensor of zeros (ideal)
     ##################################################################
+
     real_loss = F.binary_cross_entropy_with_logits(discrim_real, torch.ones_like(discrim_real))
     fake_loss = F.binary_cross_entropy_with_logits(discrim_fake, torch.zeros_like(discrim_fake))
     loss = real_loss + fake_loss
@@ -30,6 +42,10 @@ def compute_generator_loss(discrim_fake):
     ##################################################################
     # TODO 1.3: Implement GAN loss for the generator.
     ##################################################################
+    # For generator, aim is to ge the MINIMUM loss between fake and real images
+    # loss evaluated predictions of fake images and ideally real images (tensor with all ones)
+    ######
+    
     loss = F.binary_cross_entropy_with_logits(discrim_fake, torch.ones_like(discrim_fake))
     ##################################################################
     #                          END OF YOUR CODE                      #
